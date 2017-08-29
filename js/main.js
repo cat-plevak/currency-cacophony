a912a384f571487603e439835c3db935578c3c0f
cat-plevak <catherine.plevak@colorado.edu>
2017-08-29 11:23:38 -0600

    derivatives

--- js/main.js
+++ js/main.js
@@ -1,34 +1,53 @@
 // json request
 $(document).ready(function() {
   // gather JSON
-  const TIMEOUT_MS = 60000;
+  const TIMEOUT_MS = 3000;
+  const RADIUS_COEFF = 25;
 
+
+  // get json data
   function updateTick() {
     $.get('https://forex.1forge.com/1.0.2/quotes?pairs=EURUSD,CADUSD,AUDUSD,GBPUSD,NZDUSD,CHFUSD&api_key=AlZZXbw8bkmXnEiBVKr6zbgzkiKmHJKE', success);
     setTimeout(updateTick, TIMEOUT_MS);
   }
 
+
   function success(data) {
     // make bubbles
-    d3.select("#content").attr("align","center");
+    d3.select("#content").attr("align", "center");
     var width = 800;
     var height = 600;
 
     var numNodes = 7;
 
-    // var i = 0;
-    // var nodes = d3.range(numNodes).map((d) => {
-    //   var price = data[i++].price;
-    //   return {
-    //     radius: price
-    //   };
-    // });
+    var maxPrice = data.reduce((last, current) => Math.max(current.price, last), Number.MIN_SAFE_INTEGER);
 
     var nodes = data.map((el) => {
       return {
-        radius: el.price
-      }
+        radius: el.price / maxPrice * RADIUS_COEFF
+      };
     });
+
+    const ts = [];
+    const price = [];
+    const dp = [];
+
+    data.forEach((el) => {
+      ts.push(el.timestamp);
+      price.push(el.price);
+    });
+
+    function derivatives(ts, price) {
+      for(i=1; i<ts.length; i++) {
+        let derivative = (price[i] - price[i-1]) / (ts[i] - ts[i-1]);
+        dp.push(derivative);
+      }
+    }
+
+    ts.unshift();
+    price.unshift();
+
+    console.log(dp);
 
     var simulation = d3.forceSimulation(nodes)
       .force('charge', d3.forceManyBody().strength(30))
@@ -62,5 +81,5 @@ $(document).ready(function() {
 
   updateTick();
 
-  console.log(values);
+  // console.log(values);
 });
