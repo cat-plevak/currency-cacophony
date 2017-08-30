e6c30caa88b12b4818f8f8ee403dbdba078a6911
cat-plevak <catherine.plevak@colorado.edu>
2017-08-30 07:40:20 -0600

    colorized

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-// json request
+
 $(document).ready(function() {
-  // gather JSON
+  // get json every 3 sec
   const TIMEOUT_MS = 3000;
-  const RADIUS_COEFF = 25;
+  // radius multiplier
+  const RADIUS_COEFF = 100;
 
-
-  // get json data
+  // get json data on updateTick
   function updateTick() {
-    $.get('https://forex.1forge.com/1.0.2/quotes?pairs=EURUSD,CADUSD,AUDUSD,GBPUSD,NZDUSD,CHFUSD&api_key=MHvtlEXw7uWvwZMWXJoNjc3EyhqatfzK', success);
+    $.get('https://forex.1forge.com/1.0.2/quotes?pairs=EURUSD,CADUSD,AUDUSD,GBPUSD,NZDUSD,CHFUSD&api_key=8O9bzHMif3AT4u6ODy3yhRIalXnJWiI8', success);
     setTimeout(updateTick, TIMEOUT_MS);
   }
 
@@ -19,7 +19,7 @@ $(document).ready(function() {
     var height = 600;
 
 
-
+    // find max rate in array
     var maxPrice = data.reduce((last, current) => Math.max(current.price, last), Number.MIN_SAFE_INTEGER);
 
     var nodes = data.map((el) => {
@@ -48,6 +48,7 @@ $(document).ready(function() {
     //
     // console.log(dp);
 
+    // forcelayout
     var simulation = d3.forceSimulation(nodes)
       .force('charge', d3.forceManyBody().strength(30))
       .force('center', d3.forceCenter(width / 2, height / 2))
@@ -61,10 +62,16 @@ $(document).ready(function() {
         .selectAll('circle')
         .data(nodes);
 
+      var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
+      // append circles and style nodes
       u.enter()
         .append('circle')
         .attr('r', function(d) {
           return d.radius;
+        })
+        .style("fill", function(d, i) {
+          return colors[i];
         })
         .merge(u)
         .attr('cx', function(d) {
@@ -76,6 +83,7 @@ $(document).ready(function() {
 
       u.exit().remove();
     }
+
   }
 
   updateTick();
