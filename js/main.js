34beb25f278d8fe11ca1dda1931a4a5742b7b9d9
cat-plevak <catherine.plevak@colorado.edu>
2017-08-30 15:37:37 -0600

    derivative

--- js/main.js
+++ js/main.js
@@ -1,117 +1,131 @@
 $(document).ready(function() {
-  // get json every 3 sec
-  const TIMEOUT_MS = 300000;
-  // radius multiplier
-  const RADIUS_COEFF = 100;
+  // get json every 5 sec
+  const TIMEOUT_MS = 5000;
+  const ALPHA = 1e6;
+  const RADIUS_COEFF = 10;
+  let fired = false;
+  let latestRates = null;
 
+
+  // call for splash page
   openNav();
 
   // get json data on updateTick
   function updateTick() {
     $.get('https://forex.1forge.com/1.0.2/quotes?pairs=EURUSD,CADUSD,AUDUSD,GBPUSD,NZDUSD,CHFUSD&api_key=8O9bzHMif3AT4u6ODy3yhRIalXnJWiI8', success);
-    setTimeout(updateTick, TIMEOUT_MS);
+    if (!fired) {
+      setTimeout(updateTick, TIMEOUT_MS);
+      fired = true;
+    }
   }
 
 
   function success(data) {
 
-    // position content box
+    // format content box
     d3.select("#content").attr("align", "center");
     var width = 800;
     var height = 600;
 
 
-    // find max rate in array
-    var maxPrice = data.reduce((last, current) => Math.max(current.price, last), Number.MIN_SAFE_INTEGER);
 
-    // create radius for each bubble from data
-    var nodes = data.map((el) => {
-      return {
-        radius: el.price / maxPrice * RADIUS_COEFF
-      };
-    });
-
-    // const ts = [];
-    // const price = [];
-    // const dp = [];
-    //
-    // data.forEach((el) => {
-    //   ts.push(el.timestamp);
-    //   price.push(el.price);
-    // });
-    //
-    //
-    // for(i=1; i<ts.length; i++) {
-    //   let derivative = (price[i] - price[i-1]) / (ts[i] - ts[i-1]);
-    //   dp.push(derivative);
-    // }
-    //
-    // ts.unshift();
-    // price.unshift();
-    //
-    // console.log(dp);
-
-    // forcelayout
-    var simulation = d3.forceSimulation(nodes)
-      .force('charge', d3.forceManyBody().strength(30))
-      .force('center', d3.forceCenter(width / 2, height / 2))
-      .force('collision', d3.forceCollide().radius(function(d) {
-        return d.radius;
-      }))
-      .on('tick', ticked);
-
-    // select all bubbles and append node data
-    function ticked() {
-      var u = d3.select('svg')
-        .selectAll('circle')
-        .data(nodes);
-
-
-      // change bubble colors
-      var colors = ['rgba(240, 85, 31, 0.8)', 'rgba(250, 209, 85, 0.8)', 'rgba(168, 198, 178, 0.8)', 'rgba(178, 17, 67, 0.7)', 'rgba(195, 215, 138, 0.8)', 'rgba(68, 146, 168, 0.8)'];
-
-      //change transparency and show name/value on mouseover
-      $('circle').mouseover(function() {
-        $(this).css('opacity', 0.6)
-        $(this).append('<h1>hello</h1>')
-        //append el.price
-        //append name
-      })
-
-      $('circle').mouseleave(function() {
-        $(this).css('opacity', 1)
-        //append el.price
-        //append name
-      })
-
-
-      // append circles and style nodes
-      u.enter()
-        .append('circle')
-        .attr('r', function(d) {
-          return d.radius;
-        })
-        .style("fill", function(d, i) {
-          return colors[i];
-        })
-        .merge(u)
-        .attr('cx', function(d) {
-          return d.x;
-        })
-        .attr('cy', function(d) {
-          return d.y;
+    if (latestRates !== null) {
+      let withDP = [];
+      data.forEach((el, idx) => {
+        pf = el.price;
+        pi = latestRates[idx].price;
+        tf = el.timestamp;
+        ti = latestRates[idx].timestamp;
+        let dPrice = (tf - ti === 0) ? 0 : (pf - pi) / (tf - ti);
+        let alphaDPrice = ALPHA * dPrice;
+        withDP.push({
+          price: el.price,
+          timestamp: el.timestamp,
+          symbol: el.symbol,
+          tf: tf,
+          ti: ti,
+          pf: pf,
+          pi: pi,
+          dPrice: dPrice,
+          alphaDPrice: alphaDPrice
         });
+      });
+      latestRates = withDP;
+      plotLatestRates();
+    }
+    else {
+      latestRates = data;
+      console.log('FIRST PASS', latestRates);
+    }
 
 
-      u.exit().remove();
+
+    function plotLatestRates() {
+      // create radius for each bubble from data
+      var nodes = latestRates.map((el) => {
+        const radius = el.price + el.alphaDPrice * RADIUS_COEFF;
+        return {
+          radius: radius
+        };
+      });
+
+
+
+      // forcelayout
+      var simulation = d3.forceSimulation(nodes)
+        .force('charge', d3.forceManyBody().strength(30))
+        .force('center', d3.forceCenter(width / 2, height / 2))
+        .force('collision', d3.forceCollide().radius(function(d) {
+          return d.radius;
+        }))
+        .on('tick', ticked);
+
+      // select all bubbles and append node data
+      function ticked() {
+        var u = d3.select('svg')
+          .selectAll('circle')
+          .data(nodes);
+
+
+        // change bubble colors
+        var colors = ['rgba(240, 85, 31, 0.8)', 'rgba(250, 209, 85, 0.8)', 'rgba(168, 198, 178, 0.8)', 'rgba(178, 17, 67, 0.7)', 'rgba(195, 215, 138, 0.8)', 'rgba(68, 146, 168, 0.8)'];
+
+        //change transparency on mouseover
+        $('circle').mouseover(function() {
+          $(this).css('opacity', 0.6)
+        })
+
+        $('circle').mouseleave(function() {
+          $(this).css('opacity', 1)
+        })
+
+
+        // append circles and style nodes
+        u.enter()
+          .append('circle')
+          .attr('r', function(d) {
+            return d.radius;
+          })
+          .style("fill", function(d, i) {
+            return colors[i];
+          })
+          .merge(u)
+          .attr('cx', function(d) {
+            return d.x;
+          })
+          .attr('cy', function(d) {
+            return d.y;
+          });
+
+
+        u.exit().remove();
     }
 
   }
+}
 
-  updateTick();
-
+updateTick();
 });
-
 
 
 
